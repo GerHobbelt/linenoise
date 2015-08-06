4567aa33cb508b5a549039fb1b0dfc7231dacb58
Richard Howe <howe.r.j.89@gmail.com>
2015-08-06 20:23:42 +0100

    added NDEBUG

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-tcc -D__WIN32 -c libline.c -o libline.o
-tcc -D__WIN32 -c main.o -o main.o
+tcc -DNDEBUG -D__WIN32 -c libline.c -o libline.o
+tcc -DNDEBUG -D__WIN32 -c main.o -o main.o
 tcc main.o libline.o -o main.exe
