c51b04caf3e988bc67ba8ebadf80e5c21934b546
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2016-02-20 18:22:30 +0900

    show warning prompt when size of candidates is greater than threshold

--- linenoise.c
+++ linenoise.c
@@ -605,62 +605,41 @@ static int completeLine(struct linenoiseState *ls) {
     char cbuf[32]; // large enough for any encoding?
 
     completionCallback(ls->buf, ls->pos, &lc);
-    if (lc.len == 0) {
+    insertEstimatedSuffix(ls, &lc);
+    if(lc.len == 0) {
         linenoiseBeep();
+    } else if(lc.len == 1) {
+        linenoiseEditInsert(ls, " ", 1);
     } else {
-        size_t stop = 0, i = 0;
+        int show = 1;
+        if(lc.len >= 100) {
+            char msg[256];
+            snprintf(msg, 256, "\r\nDisplay all %zu possibilities? (y or n) ", lc.len);
+            write(OUTPUT_FD, msg, strlen(msg));
 
-        insertEstimatedSuffix(ls, &lc);
-        if(lc.len > 1) {
+            while(1) {
+                nread = readCode(ls->ifd,cbuf,sizeof(cbuf),&c);
+                if (nread <= 0) {
+                    freeCompletions(&lc);
+                    return -1;
+                }
+                if(c == 'y') {
+                    break;
+                } else if(c == 'n') {
+                    write(OUTPUT_FD, "\r\n", strlen("\r\n"));
+                    show = 0;
+                    break;
+                } else {
+                    linenoiseBeep();
+                }
+            }
+            c = 0;
+        }
+
+        if(show) {
             showAllCandidates(ls->cols, &lc);
-            refreshLine(ls);
-            stop = 1;
-        } else if(lc.len == 1) {
-            linenoiseEditInsert(ls, " ", 1);
-            stop = 1;
         }
-
-        while(!stop) {
-            /* Show completion or original buffer */
-            if (i < lc.len) {
-                struct linenoiseState saved = *ls;
-
-                ls->len = ls->pos = strlen(lc.cvec[i]);
-                ls->buf = lc.cvec[i];
-                refreshLine(ls);
-                ls->len = saved.len;
-                ls->pos = saved.pos;
-                ls->buf = saved.buf;
-            } else {
-                refreshLine(ls);
-            }
-
-            nread = readCode(ls->ifd,cbuf,sizeof(cbuf),&c);
-            if (nread <= 0) {
-                freeCompletions(&lc);
-                return -1;
-            }
-
-            switch(c) {
-                case 9: /* tab */
-                    i = (i+1) % (lc.len+1);
-                    if (i == lc.len) linenoiseBeep();
-                    break;
-                case 27: /* escape */
-                    /* Re-show original buffer */
-                    if (i < lc.len) refreshLine(ls);
-                    stop = 1;
-                    break;
-                default:
-                    /* Update buffer and return */
-                    if (i < lc.len) {
-                        nwritten = snprintf(ls->buf,ls->buflen,"%s",lc.cvec[i]);
-                        ls->len = ls->pos = nwritten;
-                    }
-                    stop = 1;
-                    break;
-            }
-        }
+        refreshLine(ls);
     }
 
     freeCompletions(&lc);
