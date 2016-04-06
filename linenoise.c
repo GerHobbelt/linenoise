e2f703023a38d5a687feeac8123006ff34410af1
antirez <antirez@gmail.com>
2016-04-06 13:39:09 +0200

    4096 bytes line limit removed when STDIN is not a tty.

--- linenoise.c
+++ linenoise.c
@@ -1297,26 +1297,52 @@ static int linenoiseRaw(char *buf, size_t buflen, const char *prompt) {
         errno = EINVAL;
         return -1;
     }
-    if (!isatty(INPUT_FD)) {
-        /* Not a tty: read from file / pipe. */
-        int rlen;
-        if((rlen = read(INPUT_FD, buf, buflen)) <= 0)
-            return -1;
-        buf[(unsigned int)rlen < buflen ? rlen : rlen - 1] = '\0';
-        count = (unsigned int)rlen < buflen ? rlen : rlen - 1;
-        if (count && buf[count-1] == '\n') {
-            count--;
-            buf[count] = '\0';
-        }
-    } else {
-        /* Interactive editing. */
-        if (enableRawMode(INPUT_FD) == -1) return -1;
-        count = linenoiseEdit(INPUT_FD, OUTPUT_FD, buf, buflen, prompt);
-        disableRawMode(INPUT_FD);
-        int r = write(OUTPUT_FD, "\n", 1);
-        UNUSED(r);
-    }
+
+    if (enableRawMode(INPUT_FD) == -1) return -1;
+    count = linenoiseEdit(INPUT_FD, OUTPUT_FD, buf, buflen, prompt);
+    disableRawMode(INPUT_FD);
+    int r = write(OUTPUT_FD, "\n", 1);
+    UNUSED(r);
     return count;
+}
+
+/* This function is called when linenoise() is called with the standard
+ * input file descriptor not attached to a TTY. So for example when the
+ * program using linenoise is called in pipe or with a file redirected
+ * to its standard input. In this case, we want to be able to return the
+ * line regardless of its length (by default we are limited to 4k). */
+static char *linenoiseNoTTY(void) {
+    char *line = NULL;
+    size_t len = 0, maxlen = 0;
+
+    while(1) {
+        if (len == maxlen) {
+            if (maxlen == 0) maxlen = 16;
+            maxlen *= 2;
+            char *oldval = line;
+            line = realloc(line,maxlen);
+            if (line == NULL) {
+                if (oldval) free(oldval);
+                return NULL;
+            }
+        }
+        char c;
+        if(read(INPUT_FD, &c, 1) <= 0) {
+            c = EOF;
+        }
+        if (c == EOF || c == '\n') {
+            if (c == EOF && len == 0) {
+                free(line);
+                return NULL;
+            } else {
+                line[len] = '\0';
+                return line;
+            }
+        } else {
+            line[len] = c;
+            len++;
+        }
+    }
 }
 
 /* The high level function that is the main API of the linenoise library.
@@ -1328,7 +1354,11 @@ char *linenoise(const char *prompt) {
     char buf[LINENOISE_MAX_LINE];
     int count;
 
-    if (isUnsupportedTerm()) {
+    if (!isatty(INPUT_FD)) {
+        /* Not a tty: read from file / pipe. In this mode we don't want any
+         * limit to the line size, so we call a function to handle that. */
+        return linenoiseNoTTY();
+    } else if (isUnsupportedTerm()) {
         size_t len;
 
         int r = write(OUTPUT_FD, prompt, strlen(prompt));
