936b8058977298bd6d8e41c3693f59ccf33a7df9
dbjdbj <dbj@dbj.org>
2020-04-08 13:17:11 +0200

    convert utf turned into single header C lib

--- src/linenoise.cpp
+++ src/linenoise.cpp
@@ -6,30 +6,6 @@
  *
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the name of Redis nor the names of its contributors may be used
- *     to endorse or promote products derived from this software without
- *     specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
  * line editing lib needs to be 20,000 lines of C code.
  *
  * You can find the latest source code at:
@@ -122,15 +98,17 @@
 #include <fcntl.h>
 
 #include "../include/linenoise.h"
-#include "ConvertUTF.h"
+#include "linenoise_convert_utf.h"
 
 #include <string>
-#include <vector>
-#include <memory>
-
 using std::string;
+
+#include <vector>
 using std::vector;
+
+#include <memory>
 using std::unique_ptr;
+
 using namespace linenoise_ng;
 
 typedef unsigned char char8_t;
@@ -229,7 +207,7 @@ static void copyString32to16(char16_t* dst, size_t dstSize, size_t* dstCount,
     }
   }
 }
-#endif
+#endif // _WIN32
 
 static void copyString32to8(char* dst, size_t dstSize, size_t* dstCount,
                             const char32_t* src, size_t srcSize) {
